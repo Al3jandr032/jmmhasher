b04cae4d69343e4e1295a16b7517020a13da5062
Joshua Harley <gibwar@gibixonline.com>
2014-01-12 15:20:02 -0700

    Added MD4 functionality.

--- macmain.c
+++ macmain.c
@@ -1,6 +1,13 @@
 #include "core/crc32.h"
 #include "core/md5.h"
+#include "core/md4.h"
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * Computes the CRC32 on the contents of the provided file.
@@ -28,6 +35,36 @@ void hash_file_crc32(char* filename) {
     printf("  ");
     for (int i = 0; i < 4; ++i) {
         printf("%02x", crc.hash[i]);
+    }
+    printf(" %s\n", filename);
+}
+
+/** Computes the MD4 on the contents of the provided file.
+ * @param filename The name of the file to process.
+ */
+void hash_file_md4(char* filename) {
+    int file = open(filename, O_RDONLY | O_SHLOCK);
+    if (file == -1) {
+        fprintf(stderr, "Unable to open file %s: %s\n", filename, strerror(errno));
+    }
+
+    MD4_Context md4;
+    MD4_init(&md4);
+
+    int bytesRead;
+    unsigned char data[1024];
+    while ((bytesRead = read(file, data, 1024)) != 0) {
+        MD4_update(&md4, data, bytesRead);
+    }
+
+    unsigned char result[16];
+    MD4_final(&md4, result);
+
+    close(file);
+
+    printf("  ");
+    for (int i = 0; i < 16; ++i) {
+        printf("%02x", result[i]);
     }
     printf(" %s\n", filename);
 }
@@ -77,7 +114,8 @@ int main(int argc, char** argv) {
 
     for (int i = 1; i < argc; ++i) {
         //hash_file_crc32(argv[i]);
-        hash_file_md5(argv[i]);
+        //hash_file_md5(argv[i]);
+        hash_file_md4(argv[i]);
     }
 
     return 0;
